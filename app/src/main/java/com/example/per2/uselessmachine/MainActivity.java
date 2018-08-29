8a7a86675aa5694e627b89829b5699f9213e0cf6
TH <llwth1@gmail.com>
2018-08-29 08:51:45 -0700

    added countdown timer to the switch to turn off after 5 seconds. switching off early cancels the timer

--- app/src/main/java/com/example/per2/uselessmachine/MainActivity.java
+++ app/src/main/java/com/example/per2/uselessmachine/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.per2.uselessmachine;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
@@ -10,6 +12,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private Button buttonSelfDestruct;
     private Switch switchUseless;
+    public static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked){
+                    startSwitchOffTimer();
                     Toast.makeText(MainActivity.this, "On", Toast.LENGTH_SHORT).show();
 
 
@@ -36,6 +40,25 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+    private void startSwitchOffTimer(){
+        new CountDownTimer(5000, 1000) {
+            @Override
+            public void onTick(long l) {
+                if(!switchUseless.isChecked()){
+                    Log.d(TAG, "onTick: canceling");
+                    cancel();
+                }
+
+            }
+
+            @Override
+            public void onFinish() {
+                switchUseless.setChecked(false);
+                Log.d(TAG, "onFinish: switch set to false");
+
+            }
+        }.start();
     }
 
     private void wireWidgets() {
