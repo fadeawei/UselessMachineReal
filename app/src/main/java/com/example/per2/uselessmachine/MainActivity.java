939d49121edaafa281903336ca3fffac69b9d255
TH <llwth1@gmail.com>
2018-08-31 08:10:43 -0700

    added countdown timer to the switch to turn off after 5 seconds. switching off early cancels the timer

--- app/src/main/java/com/example/per2/uselessmachine/MainActivity.java
+++ app/src/main/java/com/example/per2/uselessmachine/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.per2.uselessmachine;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
@@ -12,6 +16,8 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private Button buttonSelfDestruct;
     private Switch switchUseless;
+    private ConstraintLayout view;
+
     public static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +31,14 @@ public class MainActivity extends AppCompatActivity {
 
     private void setListeners() {
         //TODO self destruct button
+        buttonSelfDestruct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSelfDestrutSequence();
+            }
 
+
+        });
         switchUseless.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -40,6 +53,30 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+    private void startSelfDestrutSequence() {
+        buttonSelfDestruct.setEnabled(false);
+
+
+        new CountDownTimer(10000, 1000) {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onTick(long millisUntilFinished) {
+                buttonSelfDestruct.setText("Destruct in " + (millisUntilFinished/1000));
+                if((millisUntilFinished/1000)%2 == 0) {
+                    view.setBackgroundColor(Color.WHITE);
+                }
+                else{
+                    view.setBackgroundColor(Color.BLACK);
+                }
+            }
+
+            @Override
+            public void onFinish() {
+                finish();
+
+            }
+        }.start();
     }
     private void startSwitchOffTimer(){
         new CountDownTimer(5000, 1000) {
@@ -64,5 +101,6 @@ public class MainActivity extends AppCompatActivity {
     private void wireWidgets() {
         buttonSelfDestruct = findViewById(R.id.button_main_selfdestruct);
         switchUseless = findViewById(R.id.switch_main_useless);
+        view = findViewById(R.id.constraintlayout_main);
 }
 }
